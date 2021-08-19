0c86f39532545b5ada8ebfd0db02f1b468af1662
Juyeon <remake867@gmail.com>
2021-08-20 01:21:24 +0900

    feat: 시간 형식 '연-월-일 시:분:초'로 번경

--- HustleReactorKit/APIManager.swift
+++ HustleReactorKit/APIManager.swift
@@ -14,9 +14,16 @@ class APIManager {
     static let shared = APIManager()
     private let provider = MoyaProvider<APIService>()
     
-    func readTime() -> Observable<TimeModel> {
+    func readTime() -> Observable<String> {
         return provider.rx.request(.time)
             .map(TimeModel.self)
+            .map { $0.unixtime }
+            .map { Date.init(timeIntervalSince1970: TimeInterval($0)) }
+            .map {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                return dateFormatter.string(from: $0)
+            }
             .asObservable()
     }
 }
