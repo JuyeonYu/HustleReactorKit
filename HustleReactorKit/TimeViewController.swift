d297bad81a65a6d806c4db3316d88c6af1233d1b
Juyeon <remake867@gmail.com>
2021-08-19 23:02:14 +0900

    feat: 현재 시간 기능 뷰 구현
    현재 시간을 보여줄 레이블
    현재 시간을 가져올 버튼
    표시한 현재 시간을 지울 clear 버튼

--- HustleReactorKit/TimeViewController.swift
+++ HustleReactorKit/TimeViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TimeViewController: UIViewController {
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var timeButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
