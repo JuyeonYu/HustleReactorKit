75c10fd7e73928ae8c8dd3a23d1509b8b5483966
Juyeon <remake867@gmail.com>
2021-08-20 23:22:56 +0900

    feat: 유저 목록 테이블뷰에 보여주기

---
+++ 2. TableView/TableViewReactorKit/UsersViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  TableVIewReactorKit
+//
+//  Created by Juyeon on 2021/08/20.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import RxViewController
+
+class UsersViewController: UIViewController, StoryboardView {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.reactor = UsersReactor()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func bind(reactor: UsersReactor) {
+        self.rx.viewWillAppear
+            .map { _ in Reactor.Action.readUsers }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.users }
+            .bind(to: tableView.rx.items(cellIdentifier: "cell")) { indexPath, user, cell in
+                cell.textLabel?.text = user.name
+            }
+            .disposed(by: disposeBag)
+    }
+
+
+}
+
